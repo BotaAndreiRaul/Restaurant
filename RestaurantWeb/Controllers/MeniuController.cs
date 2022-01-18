f5cb882e1581e5033732965c19b0d4b38469ed12
Andrei <Andrei@DESKTOP-ORHB0VN>
2022-01-18 13:23:42 +0200

    Am creat un controller separat care se va ocupa de operatiile de creare, editare si stergere

---
+++ RestaurantWeb/Controllers/MeniuController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantWeb.Controllers
+{
+    public class MeniuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
