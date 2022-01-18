849fd6ae46ad7259711f1dd3f0a4d52db21c04f0
Andrei <Andrei@DESKTOP-ORHB0VN>
2022-01-18 15:12:39 +0200

    Am schimbat aspectul paginii printr-ul bootstrap si am facut vizibila baza de date in mvc

--- RestaurantWeb/Controllers/MeniuController.cs
+++ RestaurantWeb/Controllers/MeniuController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantWeb.Data;
+using RestaurantWeb.Models;
 
 namespace RestaurantWeb.Controllers
 {
     public class MeniuController : Controller
     {
+        private readonly MeniuDbContext mdb;
+        public MeniuController(MeniuDbContext _mdb)
+        {
+            mdb = _mdb;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Meniu> objMeniuList = mdb.menius;
+            return View(objMeniuList);
         }
     }
 }
