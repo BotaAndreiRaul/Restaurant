59929340e2de35931546a04d9de98bdc68a9bb0b
Andrei <Andrei@DESKTOP-ORHB0VN>
2022-01-18 13:16:59 +0200

    Am creat o migratie pentru a crea, updata si urmari schimbarile in baza de date

---
+++ RestaurantWeb/Migrations/20220118110313_AddMeniuToDatabase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantWeb.Migrations
+{
+    public partial class AddMeniuToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "menius",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ingredients = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_menius", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "menius");
+        }
+    }
+}
